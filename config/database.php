8808921865a2806773cc888cf2bdc9f85a512c54
elkarrisoufia <soufiaelkarri@gmail.com>
2026-06-03 07:14:51 +0100

    Fix SSL Aiven MySQL

--- config/database.php
+++ config/database.php
@@ -1,2 +1,19 @@
 <?php
-return ['default'=>env('DB_CONNECTION','mysql'),'connections'=>['mysql'=>['driver'=>'mysql','host'=>env('DB_HOST','127.0.0.1'),'port'=>env('DB_PORT','3306'),'database'=>env('DB_DATABASE','banque_populaire'),'username'=>env('DB_USERNAME','root'),'password'=>env('DB_PASSWORD',''),'charset'=>'utf8mb4','collation'=>'utf8mb4_unicode_ci','prefix'=>'','strict'=>true,'engine'=>null]],'migrations'=>['table'=>'migrations','update_date_on_publish'=>true]];
+return ['default'=>env('DB_CONNECTION','mysql'),
+'connections'=>
+['mysql' => [
+    'driver'    => 'mysql',
+    'host'      => env('DB_HOST', '127.0.0.1'),
+    'port'      => env('DB_PORT', '3306'),
+    'database'  => env('DB_DATABASE', 'defaultdb'),
+    'username'  => env('DB_USERNAME', 'avnadmin'),
+    'password'  => env('DB_PASSWORD', ''),
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+    'strict'    => true,
+    'engine'    => null,
+    'options'   => [
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ],
+],]];
