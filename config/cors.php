13eaf717f2d7088e831aaf27cea88fb2519bc4a0
elkarrisoufia <soufiaelkarri@gmail.com>
2026-06-03 22:08:51 +0100

    Fix CORS Vercel

--- config/cors.php
+++ config/cors.php
@@ -1,2 +1,16 @@
 <?php
-return ['paths'=>['api/*','sanctum/csrf-cookie'],'allowed_methods'=>['*'],'allowed_origins'=>['http://localhost:5173','http://localhost:3000','http://localhost','http://127.0.0.1:5173'],'allowed_origins_patterns'=>[],'allowed_headers'=>['*'],'exposed_headers'=>[],'max_age'=>0,'supports_credentials'=>true];
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://gestion-rdv-frontend.vercel.app',
+        'https://gestion-rdv-frontend-git-main-soufiae-s-projects.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
